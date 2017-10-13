26dfc0394c5af59f1cb312dd7fae1204b0b0c3b9
John <jonnygovish@gmail.com>
2017-10-13 14:03:49 +0300

    Add .gitignore file

--- password_locker_test.py
+++ password_locker_test.py
@@ -7,18 +7,28 @@ class TestPassword_locker(unittest.TestCase):
   """
   Test that defines test cases for the user class bevaviours
   """
+  
   def setUp(self):
-    """
-    set up method to run before each test cases
-    """
-    self.new_user = User("jgovish", "12345")
+   """
+   set up method to run before each test cases
+   """
+   self.new_user = User("jgovish", "12345")
 
   def test_init(self):
+   """
+   Test case to test if the object is initialized properly
+   """
+   self.assertEqual(self.new_user.user_name,"jgovish")
+   self.assertEqual(self.new_user.password, "12345")
+
+   def test_user_save(self):
     """
-    Test case to test if the object is initialized properly
+    Test case to test if the user object is saved into the user_list.
     """
-    self.assertEqual(self.new_user.user_name,"jgovish")
-    self.assertEqual(self.new_user.password, "12345")
+    self.new_user.user_save()
+
+    self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
   unittest.main()
