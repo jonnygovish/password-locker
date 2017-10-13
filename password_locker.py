45977de550fe8dc14bbb602bb808f928cc6a3a34
John <jonnygovish@gmail.com>
2017-10-13 12:43:13 +0300

    Add a user class and the init method

--- password_locker.py
+++ password_locker.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import random
+
+
+class User:
+  """
+  Class that generates new instances of user
+  """
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
