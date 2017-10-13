2a4d4354e2308a1f29a8513b63f4d4e6dfab299a
John <jonnygovish@gmail.com>
2017-10-13 15:23:18 +0300

    Add global variables

--- password_locker.py
+++ password_locker.py
@@ -1,17 +1,29 @@
 import random
 
+user_name =""
+user_password =""
 
 class User:
   """
   Class that generates new instances of user
   """
+  global user_name
+  global user_password
   user_list = []
-  def __init__(self, user_name, password):
+  def __init__(self, user_name, password,email):
     self.user_name = user_name
     self.password = password
+    self.email = email
   
   def user_save(self):
     """
     saves user object into user object.
     """
     User.user_list.append(self)
+
+
+class Credentials:
+  """
+  Class that generates new instances of credential object.
+  """
+  
