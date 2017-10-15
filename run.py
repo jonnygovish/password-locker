9141f1cd17d27af6ee8b034b89e7152268acab1a
John <jonnygovish@gmail.com>
2017-10-15 07:32:20 +0300

    Createa function that implements the create_user function

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.6
+
+from password_locker import User, Credentials
+
+def create_user(user_name, password, email):
+  """
+  Function to create a new user.
+  """
+  new_user = User(user_name,password, email)
+  return new_user
