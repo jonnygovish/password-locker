b6753899e634faed29d196379fc454728050b942
John <jonnygovish@gmail.com>
2017-10-15 08:03:03 +0300

    add an attribute to the credential class

--- password_locker.py
+++ password_locker.py
@@ -29,8 +29,9 @@ class Credentials:
   """
   global user_list
   credential_list =[]
-  def __init__(self, account_name, account_password):
+  def __init__(self, account_name, account_username, account_password):
     self. account_name = account_name
+    self.account_username = account_username
     self.account_password = account_password
 
   # @classmethod
